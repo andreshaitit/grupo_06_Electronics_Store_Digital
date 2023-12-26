ce600595041ef286cee83bb17aa85c62e317f7f8
JoseLeon89 <vico12leon@gmail.com>
2023-12-26 00:42:41 -0300

    modificacion de app.js, agregando paginas con sus respectivos enlaces

---
+++ app.js
@@ -0,0 +1,33 @@
+//requiere
+const express = require ('express');
+const path = require ('path');
+
+const app = express();
+const port = 3005;
+
+//configuracion
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/productDetail', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'productDetail.html'))
+})
+
+app.get('/register', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'register.html'))
+})
+
+app.get('/login', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'login.html'))
+})
+
+app.get('/productCart', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'views', 'productCart.html'))
+})
+
+app.listen(port, ()=>{
+  console.log(`Nuestra app corre en http://localhost:${port}`);
+})
