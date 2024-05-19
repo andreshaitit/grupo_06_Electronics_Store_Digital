e774041d9383a041561c054738994f00465f9a2c
andreshaitit <andreshaitit99@gmail.com>
2024-05-18 21:18:04 -0300

    solo back

--- Frontend/src/routes/product.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const productControllers = require("../controllers/productControllers");
-const router = express.Router()
-
-router.get('/', (req, res) => res.send('Entraste al apartado de productos'))
-router.get('/detalle/:id', productControllers.detail)
-
-module.exports = router;
